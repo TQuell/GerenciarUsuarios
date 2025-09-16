6e1a45998c97b763c56d0fdc30e8aadf3108d899
Thereza Raquel <therezaraquelcoimbra@gmail.com>
2025-09-16 17:57:43 -0300

    Mudei a lista de nomes de usuários para tornarem-se objetos

--- test/gerenciarUsuarios.test.js
+++ test/gerenciarUsuarios.test.js
@@ -5,13 +5,21 @@ const { expect } = require('chai');
 describe ('Testar as funções de Gestão de Usuários', function (){
     it ('validar que posso adicionar um novo nome de usuário a lista', function (){
         // 1. Add um novo nome a lista de usuários
-        addNovoUsuario('Raquel');
+        addNovoUsuario({
+            nome: 'Raquel',
+            email: 'raquel@email.com'
+        });
 
         // 2. Retornar a lista de usuários na caixa "lista de usuários"
         const listaDeUsuarios = retornarUsuarios(); 
 
-        // 3. Verificar se o novo nome está no fim da "lista de usuários"
-        expect(listaDeUsuarios.at(-1)).to.equal('Raquel');
+        // 3. Compare se o novo nome esta no fim da lista de usuários
         // at(-1) retorna o ultimo item da lista. E o primeiro nome seria at(0)
+        // expect(ListaDeUsuarios.at(-1).nome).to.equal('Raquel');
+        // expect(ListaDeUsuarios.at(-1).email).to.equal('raquel@email.com');
+        expect(listaDeUsuarios.at(-1)).to.eql({
+            nome: 'Raquel',
+            email: 'raquel@email.com'
+        });      
     });
 });
